ab0d8f06763ed21896e9bc6648c5034407721f0d
Milaskinger <salimlaboue05@gmail.com>
2025-04-08 12:46:22 +0200

    🔥 Fix total : désactivation ESLint pour Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
